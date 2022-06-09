1bf70f01a1982b66f2d134ddc892d4d8c9304a7b
syslink <oursyslink@163.com>
2022-06-10 01:48:34 +0800

    add robot

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -7,6 +7,8 @@ import { Component, Prop } from 'vue-property-decorator'
 import * as THREE from 'three'
 
 import BaseMesh from './BaseMesh.vue'
+import { GLTFLoader } from '../utils/GLTFLoader.js';
+import * as SkeletonUtils from '../utils/SkeletonUtils.js';
 
 @Component({
   name: 'Ball',
@@ -14,6 +16,7 @@ import BaseMesh from './BaseMesh.vue'
 })
 export default class Player extends BaseMesh {
   @Prop() protected teamColor!: string
+
 
   protected material!: THREE.MeshPhongMaterial
 
@@ -23,7 +26,44 @@ export default class Player extends BaseMesh {
   }
 
   mounted() {
-    this.initialize()
+    //this.initialize()
+    this.loadRobot()
+  }
+
+  loadRobot() {
+    const loader = new GLTFLoader();
+    const scene = this.scene;
+
+    loader.load( '../assets/robot/RobotExpressive.glb', function ( robot:any ) {
+
+      robot.scene.traverse( function ( object:any ) {
+
+        if ( object.isMesh ) object.castShadow = true;
+
+      } );
+      const model = SkeletonUtils.clone( robot.scene );
+      model.scale.set(.2,.2,.2);
+      model.position.set( 0, 0, 0 );
+      model.name = 'robot';
+      model.index = 1;
+      scene.add( model );
+      // const mixer = new THREE.AnimationMixer( model );
+      // const actions:object = {};
+      // for ( let i = 0; i < robot.animations.length; i++ ) {
+
+			// 		const clip:object = robot.animations[ i ];  // 获取其中一个动画
+			// 		const action:object = mixer.clipAction( clip );  // 返回动画操作对象
+			// 		actions[ clip.name ] = action;  // 关联动画名称和动作
+
+			// 		if ( emotes.indexOf( clip.name ) >= 0 || states.indexOf( clip.name ) >= 4 ) {
+
+			// 			action.clampWhenFinished = true;  	//暂停在最后一帧播放的状态
+			// 			action.loop = THREE.LoopOnce;  		//不循环播放
+
+			// 		}
+
+			// 	}
+    } );
   }
 
   generateGeometry(): THREE.CylinderGeometry {
